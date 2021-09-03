8767ac974303efa39de33dbb7091e8668a2db452
Augustinas <augustinas@spotlightdata.co.uk>
2021-09-04 00:02:51 +0100

    Feat: rever to old code

--- old/main.cr
+++
@@ -1,7 +0,0 @@
-require "crystal"
-require "./crystal_lib"
-
-node = Crystal::Parser.parse(ARGF.gets_to_end)
-visitor = CrystalLib::LibTransformer.new
-transformed = node.transform visitor
-transformed.to_s(STDOUT, emit_doc: true)
--- src/main.cr
+++ src/main.cr
@@ -1,5 +1,5 @@
-require "compiler/crystal/syntax"
-require "./crystal_lib/**"
+require "crystal"
+require "./crystal_lib"
 
 node = Crystal::Parser.parse(ARGF.gets_to_end)
 visitor = CrystalLib::LibTransformer.new
