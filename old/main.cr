5c0e7194fae10e955d58e26eca40fd6491df7e60
Augustinas <augustinas@spotlightdata.co.uk>
2021-09-03 23:41:27 +0100

    Chore: move old files

---
+++ old/main.cr
@@ -0,0 +1,7 @@
+require "crystal"
+require "./crystal_lib"
+
+node = Crystal::Parser.parse(ARGF.gets_to_end)
+visitor = CrystalLib::LibTransformer.new
+transformed = node.transform visitor
+transformed.to_s(STDOUT, emit_doc: true)
