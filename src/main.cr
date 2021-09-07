e0dabb3b9b5b95c8a4dca94cc6d7a83421a34d34
Augustinas <augustinas@spotlightdata.co.uk>
2021-09-07 17:47:33 +0100

    Feat: import and export directly to files

--- src/main.cr
+++ src/main.cr
@@ -1,7 +1,9 @@
 # require "crystal"
 require "./crystal_lib"
 
-node = Crystal::Parser.parse(ARGF.gets_to_end)
+node = Crystal::Parser.parse(File.new(ARGV[0]).gets_to_end)
 visitor = CrystalLib::LibTransformer.new
 transformed = node.transform visitor
-transformed.to_s(STDOUT, emit_doc: true)
+content = File.open(ARGV[1], "w") do |file|
+  transformed.to_s(file, emit_doc: true)
+end
